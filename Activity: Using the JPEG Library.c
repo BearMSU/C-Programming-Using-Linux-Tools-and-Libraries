1c0a0180bcbb7b2fc62e37185eafeae905d2d491
Brandon <65471994+BearMSU@users.noreply.github.com>
2023-07-16 20:33:19 -0400

    Create Activity: Using the JPEG Library.c

---
+++ Activity: Using the JPEG Library.c
@@ -0,0 +1,9 @@
+// Activity: using the JPEG library
+// My Solution (course solution also the same)
+
+#include <stdio.h>
+
+int main(void) {
+    printf("gcc -std=c11 -Wall -fmax-errors=10 -Wextra /usr/lib/libjpeg.so vizplus.c -o vizplus");
+    return 0;
+}
